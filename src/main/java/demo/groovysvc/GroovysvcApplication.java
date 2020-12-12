6f2e49a46a68d3d0ebccadf78ed2ef21f7fde498
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2020-12-11 19:50:31 -0800

    First import from project created with Spring initializr

---
+++ src/main/java/demo/groovysvc/GroovysvcApplication.java
@@ -0,0 +1,13 @@
+package demo.groovysvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroovysvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroovysvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/groovysvc/GroovysvcApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.groovysvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroovysvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
