4bb6644d596e9ac41708b54efffb4e2afb0dfd8f
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2020-12-25 21:08:39 -0800

    Cleanup LoadDatabase

--- src/main/java/demo/groovysvc/configuration/LoadDatabase.java
+++ src/main/java/demo/groovysvc/configuration/LoadDatabase.java
@@ -1,4 +1,4 @@
-package demo.groovysvc.dao;
+package demo.groovysvc.configuration;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -6,11 +6,11 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import demo.groovysvc.dao.UserRepository;
 import demo.groovysvc.model.User;
 
 @Configuration
@@ -18,11 +18,8 @@ public class LoadDatabase {
 
 	private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
-	@Autowired
-	UserRepository userRepository;
-
 	@Bean
-	CommandLineRunner initDatabase(TaskRepository repository) {
+	CommandLineRunner initDatabase(UserRepository userRepository) {
 		return args -> {
 			List<String> demoUsers = Arrays.asList("denis", "webuser", "demo");
 			for (String username: demoUsers) {
